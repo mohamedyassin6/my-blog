180806718bcebef71d261b3e9ad867f38eb42317
Mohamed Yassin <mohamedyassin360@gmail.com>
2020-08-11 18:08:27 +0200

    First commit

---
+++ src/build/precache-manifest.8a2f9e6234574e65b354815613349458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abaeeb7ad61f4f944561891a723c8f62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "608a6c941e18aa8c39e3",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "608a6c941e18aa8c39e3",
+    "url": "/static/js/main.fe3edf85.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
